183bc9800b20fe76904966105ad72dea775d6199
Leemainsw <yebom6476@gmail.com>
2025-12-01 16:10:52 +0900

    feat: mission 2 추가

--- 12.01/src/App.jsx
+++ 12.01/src/App.jsx
@@ -1,5 +1,6 @@
 // App.jsx
 import Mandoo from "./Mandoo";
+import { useState } from "react";
 
 // 아래 todos 리스트를 만두 이미지 아래에 뿌려주세요
 const mandooTodoLists = {
@@ -7,6 +8,32 @@ const mandooTodoLists = {
 };
 
 export default function App() {
+const [type, setType] = useState("mission1");
+const [number, setNumber] = useState(1);
+const [increaseNumber, setIncreaseNumber] = useState(0);
+
+if(type === "mission2") {
+  	return(
+      <main>
+      <h1>{number}</h1>
+      <button onClick={() => setNumber(number + 1)}>
+        +1
+      </button>
+      <br />
+      <input
+        type="text"
+        placeholder="숫자만 적으세요"
+        value={increaseNumber}
+        onChange={(e) => setIncreaseNumber(Number(e.target.value))}
+
+      />
+      <br />
+      <button onClick={() => setNumber(number + increaseNumber)}>
+        + {increaseNumber}
+      </button>
+    </main>
+    )
+}
   return (
     <main style={{ padding: "20px" }}>
       <h3>Mandoo Todo</h3>
@@ -18,6 +45,8 @@ export default function App() {
           </div>
         ))
       }
+
+      <button onClick={() => setType("mission2")}>mission2</button>
     </main>
   );
 }
